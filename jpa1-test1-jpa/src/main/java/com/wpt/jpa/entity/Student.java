75351b87d9ff225587760604cdd2478d13b47fec
WaiPhyoThwe <WaiPhyoThwe@DESKTOP-D596I38>
2020-06-27 08:01:44 +0630

    Third

--- jpa1-test1-jpa/src/main/java/com/wpt/jpa/entity/Student.java
+++ jpa1-test1-jpa/src/main/java/com/wpt/jpa/entity/Student.java
@@ -1,111 +1,111 @@
 package com.wpt.jpa.entity;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+import static javax.persistence.TemporalType.DATE;
+
 import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.Enumerated;
-import static javax.persistence.EnumType.STRING;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.Transient;
-import static javax.persistence.TemporalType.DATE;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.SecondaryTable;
+import javax.persistence.Enumerated;
+import static javax.persistence.EnumType.STRING;
 
-@Entity(name = "stuDB")
-@SequenceGenerator(name = "st", allocationSize = 3)
-@SecondaryTable(name = "emaildb")
+import javax.persistence.Basic;
+import javax.persistence.Column;
+
+/**
+ * Entity implementation class for Entity: Student
+ *
+ */
+@Entity
+@Table(name = "student_tbl")
+@SequenceGenerator(name = "student_seq")
 public class Student implements Serializable {
-	private enum g {
-		Female, Male
-	}
 
-	@Id	
+	public enum Gender {Male, Female}
+	   
+	@Id
+	@GeneratedValue(strategy = SEQUENCE, generator = "student_seq")
 	private int id;
-	@Column(length = 10)
-	private String name;
-	private String major;
-	@Enumerated(STRING)
-	private g gender;
-	@Column(unique = true)
-	private String year;
-	@Column(table = "emaildb")
-	private String email;
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
+	
 	@Temporal(DATE)
-	private Date dateofBirth;
-
-	public Date getDateofBirth() {
-		return dateofBirth;
-	}
-
-	public void setDateofBirth(Date dateofBirth) {
-		this.dateofBirth = dateofBirth;
-	}
-
-	private String academicYear;
+	@Column(nullable = false)
+	private Date dateOfBirth;
+	@Enumerated(STRING)	
+	@Column(length = 10)
+	private Gender gender;
+	@Basic(optional = false)
+	private String name;
+	private String phone;
+	private String email;
+	
 	@Transient
 	private boolean selected;
+	
+	private SecurityInfo security;
+	
 	private static final long serialVersionUID = 1L;
 
 	public Student() {
 		super();
-	}
-
+	}   
 	public int getId() {
 		return this.id;
 	}
 
+	public boolean isSelected() {
+		return selected;
+	}
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
 	public void setId(int id) {
 		this.id = id;
-	}
-
+	}   
 	public String getName() {
 		return this.name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}   
+	public String getPhone() {
+		return this.phone;
 	}
 
-	public String getMajor() {
-		return this.major;
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}   
+	public String getEmail() {
+		return this.email;
 	}
 
-	public void setMajor(String major) {
-		this.major = major;
+	public void setEmail(String email) {
+		this.email = email;
 	}
-
-	public String getYear() {
-		return this.year;
+	public SecurityInfo getSecurity() {
+		return security;
 	}
-
-	public void setYear(String year) {
-		this.year = year;
+	public void setSecurity(SecurityInfo security) {
+		this.security = security;
 	}
-
-	public String getAcademicYear() {
-		return this.academicYear;
-	}
-
-	public void setAcademicYear(String academicYear) {
-		this.academicYear = academicYear;
-	}
-
-	public g getGender() {
-		return gender;
-	}
-
-	public void setGender(g gender) {
-		this.gender = gender;
-	}
-
+   
 }
